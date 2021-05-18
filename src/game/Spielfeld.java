14b5d6a47b9f0b99baac110c78c408ca377b696a
RaGit25 <runjog@e.mail.de>
2021-05-18 16:14:13 +0200

    GameOver Methode fertiggestellt
    Co-Authored-By: CClaudio129 <84072969+CClaudio129@users.noreply.github.com>

--- src/game/Spielfeld.java
+++ src/game/Spielfeld.java
@@ -1,43 +1,103 @@
 package game;
 
-class Spielfeld
-{
-    int punkte;
-    
-    Block[][] feld;
-    final int groesse;
-    int anzahl;
-    Block[] index;
+class Spielfeld {
+	int punkte;
 
-    Spielfeld (int g)
-    {
-        groeße = g;
-        feld = new Block[g][g];
-        index[] = new Block[groesse*groesse];
-    }
+	Block[][] feld; // Matrix mit allen Bloecken
+	int[] index; // Matrix als Liste, vlt.unnötig
 
-    
-    public void blockErstellen(){}
-    public void verschieben(){}
-    public Boolean istVoll(){}
-    public Block Addieren(){}
+	Spielfeld(int g) {
+		feld = new Block[g][g];
+		index = new int[g * g];
+	}
+
+	public int getGroesse() {
+		return feld.length;
+	}
+
+	public int getBreite() {
+		return (int) Math.sqrt(feld.length);
+	}
+
+	public int getAnzahl() {
+		int a = 0;
+		for (int i = 0; i < getBreite(); i++) {
+			for (int j = 0; j < getBreite(); j++) {
+				if (feld[i][j] != null) {
+					a++;
+				}
+			}
+		}
+		return a;
+	}
+
+	public Boolean existiertFeld(int x, int y) { // überprüft ob ein Block existiert
+		try {
+			return (feld[x][y] != null) ? true : false;
+		} catch (ArrayIndexOutOfBoundsException e) { // wenn der Block außerhalb des Feldes liegt -> existiert nicht
+			return false;
+		}
+	}
+
+	public Boolean gameOver() {
+		int zahl = 0;
+		if (getAnzahl() >= getGroesse()) { // Prüft ob noch freie Felder da sind
+			// Einmal für jeden Block ausprobieren
+			for (int i = 0; i < getBreite(); i++) {
+				for (int j = 0; j < getBreite(); j++) {
+					// Vier Richtungen zum Schieben werden getestet
+					if (existiertFeld(i + 1, j)) {
+						if (feld[i][j].getWert() == feld[i + 1][j].getWert()) {
+							return false;
+						}
+					}
+
+					if (existiertFeld(i - 1, j)) {
+						if (feld[i][j].getWert() == feld[i - 1][j].getWert()) {
+							return false;
+						}
+					}
+
+					if (existiertFeld(i, j + 1)) {
+						if (feld[i][j].getWert() == feld[i][j + 1].getWert()) {
+							return false;
+						}
+					}
+
+					if (existiertFeld(i, j - 1)) {
+						if (feld[i][j].getWert() != feld[i][j - 1].getWert()) {
+							zahl++;
+							if (zahl == getAnzahl()) {
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return false; // bricht Methode ab
+
+	}
+
+	public void blockErstellen() {
+	}
+
+	public void verschieben() {
+	}
+
+	public Block zusammmenschieben() {
+		return null;
+	}
 }
 
-
-
-/*public int[] leereFelder(){
-        int l = groesse-anzahl;
-        int[] leereFelder = new int[l];
-        leereFelder[0] = l;
-
-        for(int i = 0; i < index.legth;i++){
-            if(index[i]==null){
-                for (int k = 0;k < leereFelder.legth;k++){
-                    if(leereFelder[k]==null){
-                        leereFelder[k] = i;
-                    }
-                }
-            }
-        }
-        return leereFelder;
-    }
+/*
+ * public int[] leereFelder(){ int l = groesse-anzahl; int[] leereFelder = new
+ * int[l]; leereFelder[0] = l;
+ * 
+ * for(int i = 0; i < index.legth;i++){ if(index[i]==null){ for (int k = 0;k <
+ * leereFelder.legth;k++){ if(leereFelder[k]==null){ leereFelder[k] = i; } } } }
+ * return leereFelder; } public int[] getIndex(){ for (int i = 0; i <
+ * getGroesse(); i++) { for (int j = 0; j < getGroesse(); j++) { if (feld[i][j]
+ * != null) { a++; } } }
+ * 
+ */
--- src/game/main.java
+++ src/game/main.java
@@ -1,18 +1,16 @@
 package game;
 
 public class main {
-
+	static Spielfeld s;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		System.out.println("E");
 		System.out.println("E2");
-		uzgvuz
-		hgv
-		hhh
-		ugvjhg
-		System.out.println("Test3");
+		System.out.println("Test2");
+		s = new Spielfeld(4);
+		System.out.println((s.gameOver()) ? "GameOver":"Es geht weiter");
 	}
 
 }
