9db5d08532fde497a2b0c260da09f6d3b7e9315f
R. John <69145397+RaGit25@users.noreply.github.com>
2021-06-18 12:35:15 +0200

    Revert "GUI test claudio"

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,213 +1,65 @@
 package game;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import javax.swing.*;
+import java.util.Scanner;
 
-public class Game extends JPanel implements KeyListener, ActionListener {
+public class Game implements KeyListener {
 
 	static Spielfeld s = new Spielfeld(4);
-
-	static Game spiel = new Game();
-
-	static JFrame gameFrame = new JFrame("2048");
-
-	static JFrame loginFrame= new JFrame("login");
-
-	static JTextField textFeld = new JTextField("namen eingeben");
-
-	static JButton loginButton = new JButton("einloggen");
-	static JButton plusButton = new JButton("+");
-	static JButton confirmButton = new JButton("bestaetigen");
-
-	static JPanel PanelfuerAccounts = new JPanel();
-	static JPanel PanelfuerKontoerstellung = new JPanel();
-
-	static String accountString[] = {"Konto auswählen","aaaaaaaaa","bbbbbbbb"};
-	static JComboBox<Object> accountAuswahlliste = new JComboBox<Object>(Game.accountString);
-
-	//Fenster, das zu Beginn geoeffnet wird
-	public static void loginGui() {
-
-
-
-		loginFrame.setSize(600, 200);
-		loginFrame.setLocationRelativeTo(null);                  //wird in der MItte d. Bildschirms geoeffnet
-		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Fenster schliesst sich und code wird beendet wenn man auf X das drueckt
-		loginFrame.setResizable(false);                           //fenstergroesse nicht veraenderbar
-
-
-		GridBagLayout layout = new GridBagLayout();						// Layoutmanager
-		GridBagConstraints gbc = new GridBagConstraints();				//
-		loginFrame.setLayout(layout);									//
-
-		loginFrame.add(PanelfuerKontoerstellung);		//fuer die Kontoerstellung benoetigten Elemente
-		PanelfuerKontoerstellung.add(textFeld);
-		PanelfuerKontoerstellung.add(confirmButton);
-		PanelfuerKontoerstellung.setVisible(false);	//unsichtbar gemacht da hier noch nicht gebraucht
-
-		gbc.gridy = 1;	//relative Koordinaten im Layoutmanager
-		gbc.gridx = 0;
-		loginFrame.add(loginButton,gbc);	// "einloggen" Knopf mit layoutmanager
-
-
-
-		PanelfuerAccounts.add(accountAuswahlliste); 		//der Accountteil hat ein Panel damit der "einloggen" Knopf zentriert ist
-		PanelfuerAccounts.add(plusButton);
-		gbc.gridy = 0;	//relative Koordinaten im Layoutmanager
-		gbc.gridx = 0;
-		loginFrame.add(PanelfuerAccounts,gbc);		// account Panel mit layoutmanager
-
-		loginFrame.setVisible(true);
-
-		loginButton.addActionListener((ActionEvent e) -> {													//auf "einloggen" wird geclickt
-			gameGui();		//das Spielfenster wird geoeffnet und das Spiel startet
-			loginFrame.dispose(); //das Loginfenster schliesst sich
-		});
-
-		plusButton.addActionListener((ActionEvent e) -> {													//auf "+" wird geclickt
-			loginButton.setVisible(false);		//Elemente vom login werden unsichtbar gemacht
-			plusButton.setVisible(false);
-			accountAuswahlliste.setVisible(false);
-
-
-			PanelfuerKontoerstellung.setVisible(true);		//Elemente von der Kontoerstellung werden sichtbar gemacht
-			textFeld.setVisible(true);
-			confirmButton.setVisible(true);
-		});
-
-
-		confirmButton.addActionListener((ActionEvent e) -> {												// auf "bestaetigen" wird geclickt
-			loginButton.setVisible(true);;		//Elemente vom login werden wieder sichtbar gemacht
-			plusButton.setVisible(true);
-			accountAuswahlliste.setVisible(true);
-
-			PanelfuerKontoerstellung.setVisible(false);		//Elemente von der Kontoerstellung werden unsichtbar gemacht
-			textFeld.setVisible(false);
-
-		});
-
-	}
-
-	public static void gameGui() {					//das Spielfenster
-
-		gameFrame.setSize(880, 1000);
-		gameFrame.setVisible(true);
-		gameFrame.setLocationRelativeTo(null);                  //wird in der MItte d. Bildschirms geoeffnet
-		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Fenster schliesst sich und code wird beendet wenn man auf X das drueckt
-		gameFrame.setResizable(false);                           //fenstergroesse nicht veraenderbar
-		gameFrame.addKeyListener(spiel);
-		gameFrame.getContentPane().add(spiel);               //sorgt dafuer, dass das GUI existiert
-
-	}
-
+	static Scanner Eingabe = new Scanner(System.in);
 	
-	// paint wird bei jedem Zug mit repaint() aufgerufen. 
-	// erstellt im Prinzip alles, was der Spieler sieht auf zwei Methoden aufgeteilt
-	 
-	public void paint(Graphics g) {
-
-		Graphics2D g2 = (Graphics2D) g;             
-		g2.setColor(Color.gray);
-		
-		g2.fillRect(30, 80, 800, 800);   
-		g.setFont(new Font("Arial", Font.PLAIN, 50));   
-		g.drawString("Punktzahl: " + s.punkte, 20, 50);            
-		g.setFont(new Font("Arial", Font.PLAIN, 20));
-		g.drawString("Drï¿½cke auf 'enter' um neu zu starten", 10, 900);
-		g.setFont(new Font("Arial", Font.PLAIN, 25));  //schriftgroesse der zahl im feld
-		if(s.gameOver()) {
-			
-			g.setColor(Color.red);
-			g.setFont(new Font("Arial", Font.PLAIN, 30));  
-			g.drawString("GAME OVER!", 10, 950);         //bei gameOver() == true erscheint dieser Text
-			
-		}
-		
-		for (int i = 0; i < s.breite; i++) {              // geht bei jedem Zug einmal durchs Feld druch und ruft felder() auf
-			for (int j = 0; j < s.breite; j++) {
-
-				felder(g, s.feld[i][j], j * 200 + 50, i * 200 + 100);
-
-			}
-
+	
+	public void keyTyped(KeyEvent e) {
+		if (e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP) {
+			s.welcheRichtung("oben");
+		} else if (e.getKeyChar() == 's' || e.getKeyCode() == KeyEvent.VK_DOWN) {
+			s.welcheRichtung("unten");
+		} else if (e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT) {
+			s.welcheRichtung("links");
+		} else if (e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			s.welcheRichtung("rechts");
 		}
 
 	}
 
-	//kuemmert sich um das Faerben der Felder
-	
-	public void felder(Graphics g, Block block, int x, int y) {
-		
-		int wert = block.getWert();
-
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.lightGray);
-		g2.fillRoundRect(x, y, 150, 150, 5, 5);     //graue, leere Felder
-		g2.setColor(Color.black);
-		if (wert > 0) {                                   //je nach Wert wird das Feld gefaerbt
-			g2.setColor(block.getFarbe());
-			g2.fillRoundRect(x, y, 150, 150, 5, 5);
-			g2.setColor(Color.black);
-			
-			g.drawString("" + wert, x + 60, y + 80);
-			
-		}
+	public void keyReleased(KeyEvent e) {
 	}
 
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP) {
-
-			s.welcheRichtung("oben");
-			gameFrame.repaint();
-
-		} else if (e.getKeyChar() == 's' || e.getKeyCode() == KeyEvent.VK_DOWN) {
-
-			s.welcheRichtung("unten");
-			gameFrame.repaint();
-
-		} else if (e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT) {
-
-			s.welcheRichtung("links");
-			gameFrame.repaint();
-
-		} else if (e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT) {
-
-			s.welcheRichtung("rechts");
-			gameFrame.repaint();
-
-		} else if (e.getKeyCode() == KeyEvent.VK_ENTER) // neues Spiel wird gestartet
-
-		{
-			s = new Spielfeld(4);
-			s.blockErstellen();
-			s.blockErstellen();
-			gameFrame.repaint();
-		}
-
 	}
 
-	public void actionPerformed(ActionEvent e) {
-	}
-
-	public void keyReleased(KeyEvent e) { //macht nichts, muss aber da sein
-	}
-
-	public void keyTyped(KeyEvent e) {    //macht nichts, muss aber da sein
-	}
-
-	
-	//GUI und zwei bloecke werden erstellt
-	
 	public static void main(String[] args) {
-
-		loginGui();
-
-		 s.blockErstellen(); 
-		 s.blockErstellen();
+		s.blockErstellen();
+		s.blockErstellen();
+		s.ausdrucken();
+		//System.out.println((s.gameOver()) ? "GameOver" : "Es geht weiter");
+		
+		try {
+            while (!s.gameOver()) {
+            	int e = Eingabe.nextInt();
+            	System.out.println("You entered int "+e);
+            	//int e = (int) (Math.random() * 8);	//Zufällige Aktionen
+            	if (e == 8 ) {	//8
+        			s.welcheRichtung("oben");
+        			s.ausdrucken();
+        		} else if (e == 2) { //2
+        			s.welcheRichtung("unten");
+        			s.ausdrucken();
+        		} else if (e == 4) { //4
+        			s.welcheRichtung("links");
+        			s.ausdrucken();
+        		} else if (e == 6) { //6
+        			s.welcheRichtung("rechts");
+        			s.ausdrucken();
+        		}
+            	e = 0;
+              Thread.sleep(50);
+              System.out.println((s.gameOver()) ? "GameOver" : "Es geht weiter");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 }
--- src/game/Spielfeld.java
+++ src/game/Spielfeld.java
@@ -11,27 +11,22 @@ class Spielfeld {
 
 	Spielfeld(int g) {
 		feld = new Block[g][g];
+
 		breite = g;
-		punkte = 0;
 
 		for (int i = 0; i < breite; i++) {
 			for (int j = 0; j < breite; j++) {
 
-				feld[i][j] = new Block(0); // Jedes Feld wird erstellt und hat den Wert 0
+				feld[i][j] = new Block(0, i, j); // Jedes Feld wird erstellt und hat den Wert 0
 				// -> diese sollten logischerweise nicht sichtbar sein
 
 			}
 			punkte = 0;
 			zuege = 0;
 			veraendert = false;
-			setAllFalse();
+			// setAllFalse();
 		}
 
-	}
-
-	public int getPunkte() {
-
-		return punkte;
 	}
 
 	public int getAnzahl() {
@@ -55,18 +50,18 @@ class Spielfeld {
 
 	}
 
-	public Boolean existiertFeld(int zeile, int spalte) { // Ueberprueft ob ein Block existiert
+	public Boolean existiertFeld(int zeile, int spalte) { // überprüft ob ein Block existiert
 		try {
 			return (feld[zeile][spalte] != null) ? true : false;
-		} catch (ArrayIndexOutOfBoundsException e) { // wenn der Block ausserhalb des Feldesliegt -> existiert nicht
+		} catch (ArrayIndexOutOfBoundsException e) { // wenn der Block außerhalb des Feldesliegt -> existiert nicht
 			return false;
 		}
 	}
 
 	public Boolean gameOver() {
 		int zahl = 0;
-		if (getAnzahl() == (breite * breite)) { // Prueft ob noch freie Felder da sind
-			// Einmal fuer jeden Block ausprobieren
+		if (getAnzahl() == (breite * breite)) { // Prüft ob noch freie Felder da sind
+			// Einmal für jeden Block ausprobieren
 			for (int i = 0; i < breite; i++) {
 				for (int j = 0; j < breite; j++) {
 					// Vier Richtungen zum Schieben werden getestet
@@ -161,8 +156,8 @@ class Spielfeld {
 	}
 
 	public void verschieben(int zeile, int spalte, int z, int s) {
-		// praktische Verbesserung: die Werte fuer z(eile) und s(palte), fuer das
-		// hinzuverschiebende Feld werden Uebergeben
+		// praktische Verbesserung: die Werte für z(eile) und s(palte), für das
+		// hinzuverschiebende Feld werden übergeben
 
 		if (existiertFeld(z, s) && feld[zeile][spalte].getWert() != 0) { // Schaut, ob es das Feld daneben und selbst
 																			// gibt
@@ -172,7 +167,7 @@ class Spielfeld {
 
 				feld[z][s].setWert(feld[zeile][spalte].getWert() * 2); // Neues Feld mit doppeltem Wert
 				feld[z][s].setVerschoben(true); // Feld als verschoben gekennzeichnet
-			
+
 				feld[zeile][spalte].setWert(0); // Altes Feld wird null gesetzt
 
 				punkte += (feld[z][s].getWert()); // Punktesystem
@@ -186,11 +181,9 @@ class Spielfeld {
 				int ze = z + (z - zeile); // Neue Zeile plus/minus 1
 				int sp = s + (s - spalte); // Neue Spalte plus/minus 1
 
-
 				veraendert = true; // Es wurde was verschoben
 
-				verschieben(z, s, ze, sp); // Verschieben neuen Feldes
-
+				verschieben(z, s, ze, sp); // Verschieben neuen Feldes unnötig, weil richtige Richtung durch Feld
 
 			}
 		}
@@ -209,10 +202,10 @@ class Spielfeld {
 
 			if (zufallszahl < 0.6) {
 
-				feld[zeile][spalte] = new Block(2);
+				feld[zeile][spalte] = new Block(2, zeile, spalte);
 			} else {
 
-				feld[zeile][spalte] = new Block(4);
+				feld[zeile][spalte] = new Block(4, zeile, spalte);
 			}
 		} else {
 			if (getAnzahl() < (breite * breite)) {
@@ -221,9 +214,8 @@ class Spielfeld {
 		}
 		// ausdrucken();
 	}
-	 
-	//Zur Fehlerbehebung
-	/*public void ausdrucken() {
+
+	public void ausdrucken() {
 		for (int k = 0; k < 5; k++) {
 			System.out.println();
 		}
@@ -239,5 +231,5 @@ class Spielfeld {
 		}
 		System.out.println("-----------------------");
 		System.out.println("Zuege:" + zuege);
-	}*/
+	}
 }
