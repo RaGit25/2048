b44a61919a8ca04e34cd1803f2b660b70d2a018f
pchernig <pchernig@gmail.com>
2021-07-03 23:38:27 +0200

    Merge branch 'prvosisorischer-branch#2' into Login-Fixen

--- src/game/Login.java
+++ src/game/Login.java
@@ -8,6 +8,7 @@ import java.awt.event.FocusEvent;
 import java.io.File;
 import java.util.ArrayList;
 import javax.swing.*;
+import java.applet.*;
 
 public class Login extends JPanel implements ActionListener {
 
@@ -15,10 +16,19 @@ public class Login extends JPanel implements ActionListener {
 
 	static JFrame loginFrame = new JFrame("login");
 
-	static ImageIcon bkgicon = new ImageIcon("bilder/" + "Login-bkg" + ".png"); // Kommt aus dem Ordner Bilder
-	static JLabel background = new JLabel(
-			new ImageIcon(bkgicon.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT)));
-	static ImageIcon titleicon = new ImageIcon("2048.png");
+	static ImageIcon[] bkgicon = new ImageIcon[7];
+
+	static ImageIcon bkg1 =  new ImageIcon("bilder/bkg1.jpg");
+	static ImageIcon bkg2 =  new ImageIcon("bilder/bkg2.jpg");
+	static ImageIcon bkg3 =  new ImageIcon("bilder/bkg3.jpg");
+	static ImageIcon bkg4 =  new ImageIcon("bilder/bkg4.jpg");
+	static ImageIcon bkg5 =  new ImageIcon("bilder/bkg5.jpg");
+	static ImageIcon bkg6 =  new ImageIcon("bilder/bkg6.jpg");
+	static ImageIcon bkg7 =  new ImageIcon("bilder/bkg7.PNG");
+
+
+
+	static ImageIcon titleicon = new ImageIcon("bilder/2048.png");
 	static JLabel title = new JLabel(
 			new ImageIcon(titleicon.getImage().getScaledInstance(150, 75, Image.SCALE_DEFAULT)));
 
@@ -34,15 +44,29 @@ public class Login extends JPanel implements ActionListener {
 
 	static String groesseString[] = { "Feldgroesse auswaehlen", "3x3", "4x4", "5x5", "6x6", "7x7", "8x8" };
 	static JComboBox<Object> groesseComboBox = new JComboBox<Object>(groesseString);
-	
+
+
+
 	public Login() {
-		
+
 		accountAuswahlliste.setEditable(true);
-		
+
 	}
 
+
 	public static void loginGui() {
-		
+
+		bkgicon[0] = bkg1;
+		bkgicon[1] = bkg2;
+		bkgicon[2] = bkg3;
+		bkgicon[3] = bkg4;
+		bkgicon[4] = bkg5;
+		bkgicon[5] = bkg6;
+		bkgicon[6] = bkg7;
+		JLabel background = new JLabel(
+				new ImageIcon(bkgicon[(int)(Math.random()*7)].getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT)));
+
+
 		loginFrame.setSize(600, 300);
 		loginFrame.setLocationRelativeTo(null); // wird in der MItte d. Bildschirms geoeffnet
 		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fenster schliesst sich und code wird beendet wenn
@@ -86,9 +110,9 @@ public class Login extends JPanel implements ActionListener {
 		background.add(loadGame, gbc);
 		
 		background.add(textFeld); // fuer die Kontoerstellung benoetigten Elemente
-		textFeld.addFocusListener(new FocusAdapter() {	//Man soll immer den gesmten Text auswählen
+		textFeld.addFocusListener(new FocusAdapter() {	//Man soll immer den gesmten Text auswï¿½hlen
 			public void focusGained(FocusEvent e) {	
-				textFeld.selectAll();		//Damit man direkt überschreibt
+				textFeld.selectAll();		//Damit man direkt ï¿½berschreibt
 			}
 		});
 		background.add(confirmButton);
@@ -102,7 +126,7 @@ public class Login extends JPanel implements ActionListener {
 		// Soll einen vorhandenen Account, der ausgewaehlt wurde, erkennen
 		// und eine neu ausgewaehlte Feldgroesse einstellen und das Spiel starten
 
-		loginButton.addActionListener((ActionEvent e) -> { // auf "einloggen" wird geclickt
+		loginButton.addActionListener((ActionEvent e) -> { 										// auf "einloggen" wird geclickt
 			if (groesseComboBox.getSelectedIndex() != 0 && accountAuswahlliste.getSelectedIndex() != 0) {
 
 				Game.setAccount(JSONVerwalter.laden(accountString[accountAuswahlliste.getSelectedIndex()]));
@@ -117,7 +141,7 @@ public class Login extends JPanel implements ActionListener {
 
 		// Soll einen ausgewaehlten Account erkennen und den Spielstand laden
 
-		loadGame.addActionListener((ActionEvent e) -> { // auf "einloggen" wird geclickt
+		loadGame.addActionListener((ActionEvent e) -> { 											// auf "spiel laden" wird geclickt
 			if (accountAuswahlliste.getSelectedIndex() != 0) {
 
 				Game.setAccount(JSONVerwalter.laden(accountString[accountAuswahlliste.getSelectedIndex()]));
@@ -126,7 +150,7 @@ public class Login extends JPanel implements ActionListener {
 			}
 		});
 
-		plusButton.addActionListener((ActionEvent e) -> { // auf "+" wird geclickt
+		plusButton.addActionListener((ActionEvent e) -> { 												// auf "+" wird geclickt
 			loginButton.setVisible(false); // Elemente vom login werden unsichtbar gemacht
 			plusButton.setVisible(false);
 			accountAuswahlliste.setVisible(false);
@@ -144,7 +168,7 @@ public class Login extends JPanel implements ActionListener {
 		// und zur JComboBox "accountAuswahlliste" diesen Account hinzufuegen
 		// und ein Spiel mit der ausgewaehlten Feldgroesse starten
 
-		confirmButton.addActionListener((ActionEvent e) -> { // auf "bestaetigen" wird geclickt
+		confirmButton.addActionListener((ActionEvent e) -> { 									// auf "bestaetigen" wird geclickt
 
 			if (textFeld.getText() != "namen eingeben" && groesseComboBox.getSelectedIndex() != 0) {
 
@@ -199,7 +223,7 @@ public class Login extends JPanel implements ActionListener {
 
 		ArrayList<String> acc = new ArrayList<String>();
 		for (int i = 0; i < fileArray.length; i++) {
-			if (fileArray[i].getPath().contains(".json")) { // Wenn die Datei .json enthält
+			if (fileArray[i].getPath().contains(".json")) { // Wenn die Datei .json enthï¿½lt
 				String name = fileArray[i].getPath();
 				name = name.substring(2, name.length() - 5); // Abschneiden des .json
 				acc.add(name);
@@ -209,14 +233,18 @@ public class Login extends JPanel implements ActionListener {
 		String[] accounts = new String[(acc.size()) + 1]; // Erstellen einer String-Liste
 		accounts[0] = "Konto auswahlen"; // Erster Eintrag
 		for (int j = 0; j < (acc.size()); j++) {
-			accounts[j + 1] = acc.get(j); // Übertragung der Liste
+
+			accounts[j + 1] = acc.get(j); // ï¿½bertragung der Liste
+
+			accounts[j+1] = acc.get(j);	//ï¿½bertragung der Liste
+
 		}
 
 		return accounts;
 	}
 
 	public static void main(String[] args) {
-
+		Login l = new Login();
 		Login.loginGui();
 
 	}
